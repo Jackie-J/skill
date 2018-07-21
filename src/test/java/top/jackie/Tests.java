e160cabb888891827d9c6447561770c300df66fd
Jackie-J <huajie_mail@163.com>
2018-07-21 16:35:29 +0800

    修改

--- src/test/java/top/jackie/Tests.java
+++ src/test/java/top/jackie/Tests.java
@@ -1,24 +1,19 @@
 package top.jackie;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.assertj.core.util.Sets;
 import org.junit.Test;
 
 public class Tests {
 
     @Test
     public void test() {
-        // String orgCode = "7";
-        // String[] parentOrgCodes = { "1", "2", "3", "4", "5", "6", };
-        // String[] parentOrgCodes = null;
-        // String[] orgCodes = null;
-        // if (parentOrgCodes != null) {
-        // orgCodes = new String[parentOrgCodes.length + 1];
-        // System.arraycopy(parentOrgCodes, 0, orgCodes, 0, parentOrgCodes.length);
-        // orgCodes[parentOrgCodes.length] = orgCode;
-        // } else {
-        // orgCodes = new String[1];
-        // orgCodes[0] = orgCode;
-        // }
-        // System.out.println(orgCodes);
+        List<String> list = new ArrayList<>();
+        Set<String> set = Sets.newHashSet(list);
+        System.out.println(set.size());
     }
 
 }
